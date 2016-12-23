7db15c8b67c5e57aa5e29401e7dc6125d809c486
Pallavi Gudipati <pallavigudipati@gmail.com>
2016-12-22 17:18:40 -0800

    Linking the Tomcat server and Sphinx

--- lightningHome/src/main/java/lightningHome/SpellRecognition.java
+++ lightningHome/src/main/java/lightningHome/SpellRecognition.java
@@ -10,7 +10,16 @@ public class SpellRecognition {
 	private static final String GRAMMAR_PATH = "src/main/resources/";
 	private static final String GRAMMAR_NAME = "hpgrammar";
 	// private static final String LANGUAGE_MODEL = "resource:/edu/cmu/sphinx/demo/dialog/weather.lm";
-
+	public static Configuration getConfiguration() {
+		Configuration configuration = new Configuration();
+		configuration.setAcousticModelPath(ACOUSTIC_MODEL);
+		configuration.setDictionaryPath(DICTIONARY_PATH);
+		configuration.setGrammarPath(GRAMMAR_PATH);
+		configuration.setUseGrammar(true);
+		configuration.setGrammarName(GRAMMAR_NAME);
+		return configuration;
+	}
+	/*
 	public static void main(String[] args) throws Exception {
 		Configuration configuration = new Configuration();
 		configuration.setAcousticModelPath(ACOUSTIC_MODEL);
@@ -26,5 +35,5 @@ public class SpellRecognition {
 			String utterance = spellRecognizer.getResult().getHypothesis();
 			System.out.println(utterance);
 		}
-	}
+	}*/
 }
